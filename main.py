44554090a9b23208fc6c7bb18fe611725092241e
Mário Vido <xvido@stuba.sk>
2023-02-16 19:17:56 +0100

    5/6 úloh

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import cv2
+import numpy as np
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
+i = 0
+while True:
+    return_value, image = camera.read()
+    cv2.imshow('image', image)
+    if cv2.waitKey(20) == 32:
+        cv2.imwrite('test' + str(i) + '.jpg', image)
+        i = i + 1
+        if i == 4:
+            img1 = cv2.imread(cv2.samples.findFile("test0.jpg"))
+            img2 = cv2.imread(cv2.samples.findFile("test1.jpg"))
+            img3 = cv2.imread(cv2.samples.findFile("test2.jpg"))
+            img4 = cv2.imread(cv2.samples.findFile("test3.jpg"))
+            vertical = np.concatenate((img1, img2), axis=0)
+            vertical1 = np.concatenate((img3, img4), axis=0)
+            horizontal = np.concatenate((vertical, vertical1), axis=1)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+            # cv2.imshow("Display window", horizontal)
+            cv2.imwrite("2x2.png", horizontal)
+            shape = horizontal.shape
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
+            kernel = np.array([[0, -1, 0],
+                               [-1, 5, -1],
+                               [0, -1, 0]])
+
+            firstQuarter = horizontal[0:int(shape[0] / 2), 0:int(shape[1] / 2)]
+            dst1 = cv2.filter2D(firstQuarter, -1, kernel)
+            horizontal[0:int(shape[0] / 2), 0:int(shape[1] / 2)] = dst1
+
+            # secondQuarter = horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])]
+            # dst2 = cv2.rotate(secondQuarter, cv2.ROTATE_90_CLOCKWISE)
+            # horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])] = dst2
+
+            red = horizontal[int(shape[0] / 2):shape[0], 0:int(shape[1] / 2)]
+            red[:, :, 0] = 0
+            red[:, :, 1] = 0
+            horizontal[int(shape[0] / 2):shape[0], 0:int(shape[1] / 2)] = red
+            cv2.imshow("Output1", horizontal)
+            print(horizontal.shape, horizontal.dtype, horizontal.size)
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:  # exit on ESC
         break
-
-vc.release()
-cv2.destroyWindow("preview")
+camera.release()
+cv2.destroyAllWindows()
