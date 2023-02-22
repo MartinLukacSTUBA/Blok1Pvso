40ba45bacc676e69afa51fee4e3e998e5cb6bb06
Mário Vido <xvido@stuba.sk>
2023-02-22 16:52:55 +0100

    finished: task1, 2x2 jpg, one vertical, one red

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ i = 0
 while True:
     return_value, image = camera.read()
     cv2.imshow('image', image)
-    if cv2.waitKey(20) == 32:
+    if cv2.waitKey(1) == 32:
         cv2.imwrite('test' + str(i) + '.jpg', image)
         i = i + 1
+        print(i)
         if i == 4:
             img1 = cv2.imread(cv2.samples.findFile("test0.jpg"))
             img2 = cv2.imread(cv2.samples.findFile("test1.jpg"))
@@ -19,20 +20,23 @@ while True:
             horizontal = np.concatenate((vertical, vertical1), axis=1)
 
             # cv2.imshow("Display window", horizontal)
+            horizontal = cv2.resize(horizontal, (900, 900))
             cv2.imwrite("2x2.png", horizontal)
             shape = horizontal.shape
 
             kernel = np.array([[0, -1, 0],
                                [-1, 5, -1],
                                [0, -1, 0]])
-
             firstQuarter = horizontal[0:int(shape[0] / 2), 0:int(shape[1] / 2)]
             dst1 = cv2.filter2D(firstQuarter, -1, kernel)
             horizontal[0:int(shape[0] / 2), 0:int(shape[1] / 2)] = dst1
 
-            # secondQuarter = horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])]
-            # dst2 = cv2.rotate(secondQuarter, cv2.ROTATE_90_CLOCKWISE)
-            # horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])] = dst2
+            secondQuarter = horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])]
+            dst2 = np.zeros((secondQuarter.shape[1], secondQuarter.shape[0], 3), dtype=np.uint8)
+            for i in range(secondQuarter.shape[0]):
+                for j in range(secondQuarter.shape[1]):
+                    dst2[j, secondQuarter.shape[0] - 1 - i] = secondQuarter[i, j]
+            horizontal[0:int(shape[0] / 2), int(shape[1] / 2):int(shape[1])] = dst2
 
             red = horizontal[int(shape[0] / 2):shape[0], 0:int(shape[1] / 2)]
             red[:, :, 0] = 0
@@ -40,7 +44,8 @@ while True:
             horizontal[int(shape[0] / 2):shape[0], 0:int(shape[1] / 2)] = red
             cv2.imshow("Output1", horizontal)
             print(horizontal.shape, horizontal.dtype, horizontal.size)
-    key = cv2.waitKey(20)
+            i = 0
+    key = cv2.waitKey(1)
     if key == 27:  # exit on ESC
         break
 camera.release()
