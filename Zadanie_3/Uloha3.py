f7636e144495b8424a0a1545a15df9824f8de57b
Martin Lukáč <xlukacm3@stuba.sk>
2023-04-05 15:05:07 +0200

    DONE - Zadanie 3.

--- Zadanie_3/Uloha3.py
+++ Zadanie_3/Uloha3.py
@@ -1,21 +1,36 @@
 import cv2
+import numpy as np
 
-# Nacitanie obrazka
-img = cv2.imread("D:\School\PVSO\Projects\Zadanie_3\obrazok.jpg")
 
-# Prevod na grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def contours(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Aplikacia binarizacie na obrazok
-_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    dx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    dy = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
 
-# Najdenie kontur v binarizovanom obrazku
-contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    mag = np.sqrt(dx ** 2 + dy ** 2)
+    angle = np.arctan2(dy, dx)
 
-# Nakreslenie najdenych kontur na obrazok
-cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+    thresh = 50
+    mag[mag < thresh] = 0
 
-# Zobrazenie vysledneho obrazku s konturami
-cv2.imshow("Kontury", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for y in range(1, mag.shape[0] - 1):
+        for x in range(1, mag.shape[1] - 1):
+            if mag[y][x] > 0:
+                theta = angle[y][x]
+                if theta < -np.pi / 4 or theta > np.pi / 4:
+                    if mag[y][x] < mag[y - 1][x] or mag[y][x] < mag[y + 1][x]:
+                        mag[y][x] = 0
+                else:
+                    if mag[y][x] < mag[y][x - 1] or mag[y][x] < mag[y][x + 1]:
+                        mag[y][x] = 0
+
+    mag = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+    cv2.imshow("Contours", mag)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+img = cv2.imread("D:/School/PVSO/Projects/Zadanie_3/obrazocek.jpg")
+contours(img)
